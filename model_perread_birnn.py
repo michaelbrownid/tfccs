7d9791022a5857ef70f9b59b4fbb98f7e81ef237
Michael Brown <mbrown@pacificbiosciences.com>
2020-01-26 09:43:38 -0800

    - cleanup. should be equal

--- model_perread_birnn.py
+++ model_perread_birnn.py
@@ -9,26 +9,28 @@ class Model():
 
         self.args = args
 
+        #### inputs
         inputs = KK.layers.Input(shape=(args.rows,args.cols,args.baseinfo), name="inputs") # [None, 16, 640, 4] The MSA
         inputsCallProp = KK.layers.Input(shape=(args.cols,1), name="inputsCallProp") # [None, 640, 1] Where POA (not true) made a call.
         readNumber = KK.layers.Input(shape=(1,), name="readNumber") # single read row to examine. TODO is this the only way to feed parameter?
         inputsCallTrue = KK.layers.Input(shape=(args.cols,1), name="inputsCallTrue") # [None, 640, 1] The true call optionally used only in loss
 
+        #### hacks
         readNumberHack = KK.layers.Lambda( lambda x: x)(readNumber) # fixes error cannot be fed and fetched
         inputsCallTrueHack = KK.layers.Lambda( lambda x: x)(inputsCallTrue) # fixes error cannot be fed and fetched
 
         #### pick out one read that is specified in readNumber
         oneread = KK.layers.Lambda( lambda xx: KK.backend.mean(xx))(readNumber)
         onereadIndex = KK.layers.Lambda( lambda xx: KK.backend.cast(xx,dtype=tf.int32))(oneread)
-        readdat = KK.layers.Lambda( lambda xx: xx[0][:, xx[1] ,:,:], name="readdat")( (inputs,onereadIndex) ) # (None,640,4)
+        readdat = KK.layers.Lambda( lambda xx: xx[0][:, xx[1] ,:,:], name="readdat")( (inputs,onereadIndex) ) # (None,640,4). inputs only at readNumber
 
         #### take the baseint and embed.
-        readbaseint = KK.layers.Lambda( lambda xx: xx[:,:,0], name="baseint" )(readdat) # [None, 640]
+        readbaseint = KK.layers.Lambda( lambda xx: xx[:,:,0], name="baseint" )(readdat) # [None, 640] # really a float but integer-only values
         embedBase =   KK.layers.Embedding(input_dim=1, output_dim=8, name="embed")(readbaseint) # [None, 640, 8]
 
         #### single readNumber in so which read can influence model readNumber(None,1)->(None,640,1)
         readNumberExp = KK.layers.Lambda( lambda xx: KK.backend.expand_dims(xx, axis=1), name="readNumberExp")( readNumber ) # (None,1,1)
-        readNumberTile = KK.layers.Lambda( lambda xx: KK.backend.tile(xx, (1, 640 ,1) ), name="readNumberTile")( readNumberExp ) # (None,640,1) AttributeError: 'str' object has no attribute 'cols'
+        readNumberTile = KK.layers.Lambda( lambda xx: KK.backend.tile(xx, (1, 640 ,1) ), name="readNumberTile")( readNumberExp ) # (None,640,1)
  
         #### coverage over all input subreads at each column, so the model can know approx how many reads went into the POA, therefore POA accuracy
         colNotEmpty = KK.layers.Lambda( lambda xx: KK.backend.cast( KK.backend.not_equal( xx[:,:,:,0],0.0 ), dtype=tf.float32), name="colNotEmpty")(inputs) # [None,16,640]
@@ -40,7 +42,7 @@ class Model():
         #### forwardsense RNN (?, 640, rnn_hidden_size)
         rnn_hidden_size= 256
 
-        #### try GRU->LSTM or SimpleRNN
+        #### The RNN layers
         # 0th RNN layer
         forwardNotBack0 = KK.layers.GRU( rnn_hidden_size, return_sequences=True, go_backwards=False, name="forwardNotBack0")(readdatConcat)
         forwardYesBack0 = KK.layers.GRU( rnn_hidden_size, return_sequences=True, go_backwards=True, name="forwardYesBack0")(readdatConcat) 
@@ -62,24 +64,6 @@ class Model():
         predHPCall0=  KK.layers.Dense(128, activation='elu',name="predHPCall0")(rnnconcat)
         predHPCall =  KK.layers.Dense(2, activation='softmax',name="predHPCall")(predHPCall0) # [None, 640, 2]
 
-        if False:
-            #### make uniform loss everywhere there is no base covering
-            basecovered    = KK.layers.Lambda( lambda xx: KK.backend.expand_dims( KK.backend.cast( KK.backend.not_equal( xx[:,:,0], 0.0 ), dtype=tf.float32), axis=-1 ), name="basecovered")(   readdat)
-            notbasecovered = KK.layers.Lambda( lambda xx: 1.0-xx, name="notbasecovered")(basecovered)
-
-            #### !!! Not KK.layers.Multiply() and Add() !!!
-            #predHPUniform = KK.backend.constant( 1.0/133.0, dtype='float32', shape=[batchsize,args.cols,133], name="predHPUniform" )
-            predHPUniform = KK.backend.ones_like(predHP)*(1.0/133.0)
-            ma = tf.multiply( basecovered,predHP)
-            mb = tf.multiply( notbasecovered,predHPUniform)
-            predHPCovered =  tf.add( ma, mb) 
-
-            #predHPCallUniform = KK.backend.constant( 1.0/2.0, dtype='float32',   shape=(batchsize,args.cols,2),  name="predHPCallUniform" )
-            predHPCallUniform = KK.backend.ones_like(predHPCall)*(1.0/2.0)
-            mc = tf.multiply(basecovered,predHPCall)
-            md = tf.multiply(notbasecovered,predHPCallUniform)
-            predHPCallCovered = tf.add( mc, md )
-            
         ################################
         self.model = KK.models.Model(inputs=[inputs,inputsCallProp,inputsCallTrue,readNumber],
                                      outputs=[predHP,predHPCall,inputsCallTrueHack,readNumberHack]) 
@@ -105,35 +89,11 @@ class Model():
         #myopt = KK.optimizers.SGD()
         myopt = KK.optimizers.Adam()
 
-        #### TODO: BUG!!! OLD work has y_pred and y_true SWITCHED!!!!!
-        #### compute loss only where true call is made. kl only at calltrue==1. don't penalize outside real calls
-
-        # def loss_sparse_kl_closure( callTrue,baseint ):
-        #     # compute exponential weights over 33 possibilities, kmers drop off exponentially
-        #     #expweights = np.power(2.0, range(0,33))
-        #     def sparse_kl(y_true,y_pred):
-        #         eps = 1.0E-9 # 0.0 or too small causes NAN loss!
-        #         #kl = (y_true+eps)*(tf.math.log(y_true+eps)-tf.math.log(y_pred+eps))
-        #         kl = (y_true)*(-tf.math.log(y_pred+eps))
-        #         klsum = tf.reduce_sum(kl,axis=-1, keepdims=True) # sum up kl components, only 1 non-zero
-
-        #         # only take loss where there is a true call and there is a base
-        #         sparsekl1 = tf.multiply(klsum, callTrue) # callTrue is 0/1
-        #         sparsekl = tf.multiply(sparsekl1, KK.backend.cast(readdat[:,:,0]!=0.0,'float32')) # only where baseint!=0
-
-        #         print("*kl.shape",kl.shape)
-        #         print("*klsum.shape",klsum.shape)
-        #         print("*callTrue.shape",callTrue.shape)
-        #         print("*sparsekl.shape",sparsekl.shape)
-        #         return(sparsekl)
-        #     return(sparse_kl)
-        # #myloss = loss_sparse_kl_closure(inputsCallTrue,readdat)
-
         def myclosure( numonehot ):
             def my_sparse_categorical_crossentropy( y_true, y_pred ):
                 """Compute sparse_categorical_crossentropy: y_pred = probDistribution, y_true=Integer
 
-                Only where the read is covered: basedat!=0
+                Only where the read is covered: readbaseint!=0
                 """
 
                 y_true= KK.backend.cast( y_true[:,:,0],'int32') # so must take the integer at [0] AND feed as [batch,col,1]!
@@ -146,7 +106,6 @@ class Model():
                 # *y_true.dtype <dtype: 'int32'>
                 # *y_pred.shape (?, 640, 133)
                 # *y_pred.dtype <dtype: 'float32'>
-                # Traceback (most recent call last):
 
                 eps = 1.0E-9 # 0.0 or too small causes NAN loss!
 
@@ -161,14 +120,11 @@ class Model():
 
                 # only take loss where there is a true call and there is a base
                 # this is a closure because it refers to readbaseint outside
-                #sparsekl1 = tf.multiply(kl, inputsCallTrue) # callTrue is 1 when true call is made
-                present = KK.backend.expand_dims(KK.backend.cast( KK.backend.not_equal( readdat[:,:,0], 0.0 ), dtype=tf.float32),axis= -1)
-                print("present.shape",present.shape)
+                #####sparsekl1 = tf.multiply(kl, inputsCallTrue) # callTrue is 1 when true call is made
+                present = KK.backend.expand_dims(KK.backend.cast( KK.backend.not_equal( readbaseint, 0.0 ), dtype=tf.float32),axis= -1)
+                print("*present.shape",present.shape)
                 sparsekl = tf.multiply(kl, present ) # only where readbaseint!=0
                 print("*sparsekl.shape",sparsekl.shape)
-
-                #print("*inputsCallTrue.shape",inputsCallTrue.shape)
-                #print("*sparsekl1.shape",sparsekl1.shape)
 
                 return( tf.reduce_mean(sparsekl))
 
